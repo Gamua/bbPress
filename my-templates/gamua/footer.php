96a1778e1fbfe74e8e7d86e771008041e0893966
Holger Weissböck <holger@gamua.com>
2013-11-14 09:51:27 +0100

    Added driftwood-style scroll-aware information about Flox to forum topics/threads.

--- my-templates/gamua/footer.php
+++ my-templates/gamua/footer.php
@@ -20,5 +20,99 @@ bb_number_format_i18n( $bbdb->num_queries )
 
 <?php do_action('bb_foot'); ?>
 
+<?php
+        //This adds the information about Flox being the official game backend
+        //for Starling to forum topics/threads as a driftwood-like scroll-aware
+        //banner on the left-hand side of the topic. There is a single dependency
+        //on jQuery. All driftwood-relevant code is below this line.
+
+        $forum_name_parts = explode(" ", strtolower(bb_get_option('name')));
+        $forum_name = $forum_name_parts[0];
+        $is_test_forum = $forum_name == "gamua";
+        $is_starling_forum = $forum_name == "starling";
+        $is_sparrow_forum = $forum_name == "sparrow";
+
+        if (bb_is_topic() && ($is_test_forum || $is_starling_forum)) {
+?>
+    <style type="text/css">
+        #driftwood {
+            display: none;
+            overflow: hidden;
+            position: fixed;
+
+            width: 120px;
+            background-color: #eee;
+            -moz-border-radius: 8px;
+            -khtml-border-radius: 8px;
+            -webkit-border-radius: 8px;
+            border-radius: 8px;
+
+            padding-top: 20px;
+        }
+
+        #driftwood * {
+            display: block;
+            overflow: auto;
+            margin: auto;
+            font-size: 14px;
+            line-height: 21px;
+            text-align: center;
+            color: black;
+
+            /*ie, i heart u*/
+            border: none;
+            outline: none;
+        }
+
+        #driftwood:hover {
+            background-color: #ddd;
+        }
+
+        #driftwood:hover > a {
+            text-decoration: none;
+        }
+    </style>
+
+    <div id="driftwood">
+        <a href="http://gamua.com/flox" title="Flox - The No-Fuzz Game Backend">
+            <div>
+                <img width="100" height="37" src="<?php echo bb_get_active_theme_uri() . "images/driftwood/flox-logo.png" ?>"/>
+            </div>
+            <div style="margin-top:20px; margin-bottom:20px; padding-left:10px; padding-right:10px;">
+                The No-Fuzz Game Backend For Starling
+            </div>
+            <div>
+                <img width="120" height="152" src="<?php echo bb_get_active_theme_uri() . "images/driftwood/birds-looking-up.png" ?>"/>
+            </div>
+        </a>
+    </div>
+
+    <script type="text/javascript" defer="defer" async="async">
+        $("document").ready(function () {
+            var win = $(window);
+            var driftwood = $("#driftwood");
+            var driftwoodVisible = false;
+            var driftwoodY = 150;
+            var contentWidthHalf = 400;
+
+            var positionDriftwood = function () {
+                var left = win.width() / 2 + contentWidthHalf;
+                var top = Math.min(driftwoodY, Math.max(20, driftwoodY - win.scrollTop()));
+
+                driftwood.css("left", left).css("top", top);
+
+                if (!driftwoodVisible) {
+                    driftwoodVisible = true;
+                    driftwood.show();
+                }
+            };
+
+            win.resize(positionDriftwood);
+            win.scroll(positionDriftwood);
+            positionDriftwood();
+        });
+    </script>
+<?php } ?>
+
 </body>
 </html>
