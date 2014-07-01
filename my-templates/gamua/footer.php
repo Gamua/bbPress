0635d7d1e60b2796a8ac41178c0bcd1d8dbe8b65
Daniel Sperl <daniel@gamua.com>
2014-07-01 13:34:43 +0000

    added iTunes Affiliate auto link maker

--- my-templates/gamua/footer.php
+++ my-templates/gamua/footer.php
@@ -114,5 +114,18 @@ bb_number_format_i18n( $bbdb->num_queries )
     </script>
 <?php } ?>
 
+<script type='text/javascript'>
+    var _merchantSettings = _merchantSettings || [];
+    _merchantSettings.push(['AT', '10lJ37'], ['CT', 'Forum']);
+    (function () {
+        var autolink = document.createElement('script');
+        autolink.type = 'text/javascript';
+        autolink.async = true;
+        autolink.src = ('https:' == document.location.protocol) ? 'https://autolinkmaker.itunes.apple.com/js/itunes_autolinkmaker.js' : 'http://autolinkmaker.itunes.apple.com/js/itunes_autolinkmaker.js';
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(autolink, s);
+    })();
+</script>
+
 </body>
 </html>
