391ba6326715beed320a57a1886b5df003153729
Daniel Sperl <daniel@gamua.com>
2015-02-16 09:17:35 +0000

    now using Google custom search

--- my-templates/gamua/search.php
+++ my-templates/gamua/search.php
@@ -1,45 +1,41 @@
 <?php bb_get_header(); ?>
-
 <div class="bbcrumb"><a href="<?php bb_uri(); ?>"><?php bb_option('name'); ?></a> &raquo; <?php _e('Search')?></div>
-<?php bb_topic_search_form(); ?>
 
-<?php if ( !empty ( $q ) ) : ?>
-<h3 id="search-for"><?php _e('Search for')?> &#8220;<?php echo esc_html($q); ?>&#8221;</h3>
-<?php endif; ?>
+<br/>
+<br/>
 
-<?php if ( $recent ) : ?>
-<div id="results-recent" class="search-results">
-	<h4><?php _e('Recent Posts')?></h4>
-	<ol>
-<?php foreach ( $recent as $bb_post ) : ?>
-		<li<?php alt_class( 'recent' ); ?>>
-			<a href="<?php post_link(); ?>"><?php topic_title($bb_post->topic_id); ?></a>
-			<span class="freshness"><?php printf( __('Posted %s'), bb_datetime_format_i18n( bb_get_post_time( array( 'format' => 'timestamp' ) ) ) ); ?></span>
-			<p><?php echo bb_show_context($q, $bb_post->post_text); ?></p>
-		</li>
-<?php endforeach; ?>
-	</ol>
+<?php
+
+$forum_name = explode(" ", strtolower(bb_get_option('name')))[0];
+$custom_search_id;
+
+if ($forum_name == "starling")
+  $custom_search_id = "005966533546828650596:8edtpwdmbeg";
+else // sparrow
+  $custom_search_id = "005966533546828650596:oxrylrdroaa";
+
+?>
+
+<div>
+	<script>
+	  (function() {
+	    var cx = '<?php echo($custom_search_id) ?>';
+	    var gcse = document.createElement('script');
+	    gcse.type = 'text/javascript';
+	    gcse.async = true;
+	    gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
+	        '//www.google.com/cse/cse.js?cx=' + cx;
+	    var s = document.getElementsByTagName('script')[0];
+	    s.parentNode.insertBefore(gcse, s);
+	  })();
+	</script>
+	<gcse:searchbox></gcse:searchbox>
 </div>
-<?php endif; ?>
 
-<?php if ( $relevant ) : ?>
-<div id="results-relevant" class="search-results">
-	<h4><?php _e('Relevant posts')?></h4>
-	<ol>
-<?php foreach ( $relevant as $bb_post ) : ?>
-		<li<?php alt_class( 'relevant' ); ?>>
-			<a href="<?php post_link(); ?>"><?php topic_title($bb_post->topic_id); ?></a>
-			<span class="freshness"><?php printf( __('Posted %s'), bb_datetime_format_i18n( bb_get_post_time( array( 'format' => 'timestamp' ) ) ) ); ?></span>
-			<p><?php post_text(); ?></p>
-		</li>
-<?php endforeach; ?>
-	</ol>
+<div>
+	<gcse:searchresults></gcse:searchresults>
 </div>
-<?php endif; ?>
 
-<?php if ( $q && !$recent && !$relevant ) : ?>
-<p><?php _e('No results found.') ?></p>
-<?php endif; ?>
-<br />
-<p><?php printf(__('You may also try your <a href="http://google.com/search?q=site:%1$s %2$s">search at Google</a>'), bb_get_uri(null, null, BB_URI_CONTEXT_TEXT), urlencode($q)) ?></p>
 <?php bb_get_footer(); ?>
+
+
